2f8c033bce42a20c2f94ced7a9476d7ec6054f62
Saurabh Verma <saurabh1088@gmail.com>
2025-09-14 11:55:19 +0530

    Added project SimulatorsHelper initiator project.

---
+++ SimulatorsHelper/SimulatorsHelperTests/SimulatorsHelperTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimulatorsHelperTests.swift
+//  SimulatorsHelperTests
+//
+//  Created by Saurabh Verma on 14/09/25.
+//
+
+import Testing
+@testable import SimulatorsHelper
+
+struct SimulatorsHelperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
