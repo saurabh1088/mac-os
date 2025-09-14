518a3a7d136ac89240baec7b7acb2a9b351b7422
Saurabh Verma <saurabh1088@gmail.com>
2025-09-14 19:47:03 +0530

    Testing navigation option in macOS based app.

--- SimulatorsHelper/SimulatorsHelper/ContentView.swift
+++ SimulatorsHelper/SimulatorsHelper/ContentView.swift
@@ -8,10 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var didSelectOptionOne: Bool = false
+    @State private var didSelectOptionTwo: Bool = false
 
     var body: some View {
-        VStack {
-            Text("Hello macOS!")
+        NavigationSplitView {
+            VStack {
+                Button {
+                    didSelectOptionOne.toggle()
+                } label: {
+                    Text("Simulators")
+                }
+                
+                Button {
+                    didSelectOptionTwo.toggle()
+                } label: {
+                    Text("Others")
+                }
+
+            }
+        } detail: {
+            if didSelectOptionOne {
+                VStack {
+                    Image(systemName: "iphone.gen2.motion")
+                    Text("All the simulators we have")
+                }
+            }
+            if didSelectOptionTwo {
+                VStack {
+                    Image(systemName: "arrow.2.circlepath.circle")
+                    Text("Other useful stuff")
+                }
+            }
         }
     }
 }
