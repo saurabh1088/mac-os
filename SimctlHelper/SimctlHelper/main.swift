a877ecbf4aa19052800ae61b260b226102e783e0
Saurabh Verma <saurabh1088@gmail.com>
2025-09-16 07:28:03 +0530

    Added function listSimulators.

--- SimctlHelper/SimctlHelper/main.swift
+++ SimctlHelper/SimctlHelper/main.swift
@@ -7,5 +7,32 @@
 
 import Foundation
 
-print("Hello, World!")
+func listSimulators() {
+    let process = Process()
+    process.executableURL = URL(fileURLWithPath: "/usr/bin/xcrun")
+    process.arguments = ["simctl", "list", "devices", "available", "--json"]
+    
+    let pipe = Pipe()
+    process.standardOutput = pipe
+    process.standardError = pipe
+    
+    do {
+        try process.run()
+    } catch {
+        print("❌ Failed to run process: \(error)")
+        return
+    }
+    
+    process.waitUntilExit()
+    
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    if let output = String(data: data, encoding: .utf8) {
+        print("📱 Available Simulators:\n")
+        print(output)
+    } else {
+        print("❌ Could not read output")
+    }
+}
+
+listSimulators()
 
