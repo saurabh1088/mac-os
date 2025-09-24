a9bbed1cde2aaf7dd796d679fc1d9fe39251d588
Saurabh Verma <saurabh1088@gmail.com>
2025-09-24 21:39:17 +0530

    Added some configurations for HStack example view.

--- MacOSLearnings/MacOSLearnings/UIComponents/ContainerViews/HStackExamplesView.swift
+++ MacOSLearnings/MacOSLearnings/UIComponents/ContainerViews/HStackExamplesView.swift
@@ -8,13 +8,32 @@
 import SwiftUI
 
 struct HStackExamplesView: View {
+    @State private var verticalAlignment: VerticalAlignment = .center
+    @State private var spacing: CGFloat = 2
+    
     var body: some View {
-        HStack {
-            Text("This")
-            Text("is")
-            Text("placed in")
-            Text("a")
-            Text("HStack")
+        VStack {
+            Divider()
+            HStack(alignment: verticalAlignment, spacing: spacing) {
+                Text("This")
+                Text("is")
+                Text("placed in")
+                Text("a")
+                Text("HStack")
+            }
+            Divider()
+            HStack {
+                Button("Vertical Alignment") {
+                    verticalAlignment = .top
+                }
+                Button("Add spacing") {
+                    if spacing < 100 {
+                        spacing = spacing + 10
+                    } else {
+                        spacing = 2
+                    }
+                }
+            }
         }
     }
 }
