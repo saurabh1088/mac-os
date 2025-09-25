98761c392111221a85bbda19dab599d1e01f8dd4
Saurabh Verma <saurabh1088@gmail.com>
2025-09-25 19:57:48 +0530

    Added VStack configuration options.

--- MacOSLearnings/MacOSLearnings/UIComponents/ContainerViews/VStackExamplesView.swift
+++ MacOSLearnings/MacOSLearnings/UIComponents/ContainerViews/VStackExamplesView.swift
@@ -8,16 +8,48 @@
 import SwiftUI
 
 struct VStackExamplesView: View {
+    @State private var horizontalAlignment: HorizontalAlignment = .center
+    @State private var spacing: CGFloat = 2
+    
     var body: some View {
         VStack {
-            Image(systemName: "square.stack.3d.up.fill")
-                .resizable()
-                .frame(width: 50, height: 50)
-            Text("This")
-            Text("is")
-            Text("placed in")
-            Text("a")
-            Text("VStack")
+            Divider()
+            VStack(alignment: horizontalAlignment, spacing: spacing) {
+                Image(systemName: "square.stack.3d.up.fill")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                Text("This")
+                Text("is")
+                Text("placed in")
+                Text("a")
+                Text("VStack")
+            }
+            Divider()
+            
+            HStack {
+                Button("Horizontal Alignment Center") {
+                    horizontalAlignment = .center
+                }
+                Button("Horizontal Alignment Leading") {
+                    horizontalAlignment = .leading
+                }
+                Button("Horizontal Alignment List Row Separator Leading") {
+                    horizontalAlignment = .listRowSeparatorLeading
+                }
+                Button("Horizontal Alignment List Row Separator Trailing") {
+                    horizontalAlignment = .listRowSeparatorTrailing
+                }
+                Button("Horizontal Alignment Trailing") {
+                    horizontalAlignment = .trailing
+                }
+                Button("Add spacing") {
+                    if spacing < 100 {
+                        spacing = spacing + 10
+                    } else {
+                        spacing = 2
+                    }
+                }
+            }
         }
     }
 }
