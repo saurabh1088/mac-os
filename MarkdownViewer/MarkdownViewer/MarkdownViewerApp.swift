baeeed33453c820deb4f7693104ce5679ca6e367
Saurabh Verma <saurabh1088@gmail.com>
2026-02-17 23:07:17 +0530

    macOS Project - MarkdownViewer - KickOff Commit - 17th February 2026

---
+++ MarkdownViewer/MarkdownViewer/MarkdownViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownViewerApp.swift
+//  MarkdownViewer
+//
+//  Created by Saurabh Verma on 17/02/26.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MarkdownViewer/MarkdownViewerTests/MarkdownViewerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownViewerTests.swift
+//  MarkdownViewerTests
+//
+//  Created by Saurabh Verma on 17/02/26.
+//
+
+import Testing
+@testable import MarkdownViewer
+
+struct MarkdownViewerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
